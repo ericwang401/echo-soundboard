68ff387e1d69b03998dc8284edf4d672e79f68d4
Eric Wang <eric.wang401@gmail.com>
2022-02-01 20:05:41 -0600

    Fix hover alignment

--- src/components/AudioCard.tsx
+++ src/components/AudioCard.tsx
@@ -8,14 +8,13 @@ const AudioCard = (props: AudioCardProps) => {
   return (
     <div className='relative flex items-center bg-black border-neutral-800 border rounded-md p-4'>
       <div className='grid place-items-center absolute w-full h-full -ml-4 transition-opacity opacity-0 hover:opacity-100 bg-gray-800 rounded-md'>
-        <div className='flex w-full'>
-          <div className='flex-grow'></div>
-          <div className='flex flex-grow-0 items-center'>
+        <div className='grid grid-cols-3 w-full'>
+          <div className='col-start-2 flex items-center'>
             <PlayIcon className='w-7 h-7' />{' '}
             <span className='ml-1.5'>Play</span>
           </div>
-          <div className='flex justify-end flex-grow'>
-              <button className="p-2 m-2 rounded-md transition-colors bg-transparent hover:bg-gray-900"><DotsVerticalIcon className="w-5" /></button>
+          <div className='col-start-3 flex justify-end'>
+              <button className="p-2  m-2  ml-0 rounded-md transition-colors bg-transparent hover:bg-gray-900"><DotsVerticalIcon className="w-5" /></button>
           </div>
         </div>
       </div>
