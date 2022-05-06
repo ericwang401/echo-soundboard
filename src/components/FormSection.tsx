7a023a1c59c089ec3358f71d74dabbdb513a087b
Eric Wang <eric.wang401@gmail.com>
2022-05-06 16:59:28 -0500

    Add mascot

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -4,13 +4,16 @@ interface FormSectionProps {
   form: ReactNode
   title: string
   actions?: ReactNode
+  removePadding?: boolean
+  noTitle?: boolean
+  classNames?: string
 }
 
 const FormSection = (props: FormSectionProps) => {
   return (
-    <div className='p-6 bg-black border-neutral-700 border rounded-md'>
-        <h3 className='text-xl font-semibold'>{props.title}</h3>
-      <div className='py-3 space-y-2'>{props.form}</div>
+    <div className={`${props.removePadding ? '' : 'p-6'} relative bg-black border-neutral-700 border rounded-md`}>
+      {!props.noTitle && <h3 className='text-xl font-semibold'>{props.title}</h3>}
+      <div className={`${props.noTitle ? '' : 'py-3'} space-y-2`}>{props.form}</div>
     </div>
   )
 }
--- src/pages/Settings/SettingsContainer.tsx
+++ src/pages/Settings/SettingsContainer.tsx
@@ -5,6 +5,9 @@ import AppDataDirectory from '@/pages/settings/AppDataDirectory'
 import FileDirectory from '@/pages/settings/FileDirectory'
 import Microphone from '@/pages/settings/Microphone'
 import Outputs from '@/pages/settings/Outputs'
+import Mascot from '@/assets/images/mascot.jpg'
+import { ExclamationCircleIcon } from '@heroicons/react/solid'
+
 
 const Settings = () => {
   return (
@@ -12,18 +15,73 @@ const Settings = () => {
       <div className='content py-4 space-y-6'>
         <FormSection
           title='Info'
+          removePadding
+          noTitle
           form={
-            <p className='text-sm'>
-              In the settings page, the microphone injector DOES NOT WORK.
-              <br />
-              Soundboard made by <span className="font-bold">Eric Wang</span>
-              <br />
-              Check me out on <button onClick={() => ipcRenderer.sendSync('open-external-link', 'https://github.com/ericwang401')} className='font-bold bg-transparent'>Github</button>
-              <br />
-              <button onClick={() => ipcRenderer.sendSync('open-external-link', 'https://www.patreon.com/performave')} className='font-bold bg-transparent'>Donate to my Patreon</button>
-              <br />
-              <button onClick={() => ipcRenderer.sendSync('open-external-link', 'https://github.com/ericwang401/echo-soundboard')} className='font-bold bg-transparent'>Project source code</button>
-            </p>
+            <div className='grid grid-cols-2'>
+              <div className='p-6 space-y-2'>
+                <span>
+                  <h3 className='text-xl font-semibold'>Echo Soundboard</h3>
+                  <p className='text-sm'>
+                    A Performave product &#8226; Version 1.2.0 Yae Miko
+                  </p>
+                </span>
+
+                <div className='flex items-center text-sm space-x-3 w-full p-2 rounded-md bg-yellow-800'>
+                    <ExclamationCircleIcon className='w-5 h-5' /> <span>Keybinds are disabled when viewing settings</span>
+                </div>
+                <p className='text-sm'>
+                  <br />
+                  Soundboard made by{' '}
+                  <span className='font-bold'>Eric Wang</span>
+                  <br />
+                  Check me out on{' '}
+                  <button
+                    onClick={() =>
+                      ipcRenderer.sendSync(
+                        'open-external-link',
+                        'https://github.com/ericwang401'
+                      )
+                    }
+                    className='font-bold bg-transparent'
+                  >
+                    Github
+                  </button>
+                  <br />
+                  <button
+                    onClick={() =>
+                      ipcRenderer.sendSync(
+                        'open-external-link',
+                        'https://www.patreon.com/performave'
+                      )
+                    }
+                    className='font-bold bg-transparent'
+                  >
+                    Donate to my Patreon
+                  </button>
+                  <br />
+                  <button
+                    onClick={() =>
+                      ipcRenderer.sendSync(
+                        'open-external-link',
+                        'https://github.com/ericwang401/echo-soundboard'
+                      )
+                    }
+                    className='font-bold bg-transparent'
+                  >
+                    Project source code
+                  </button>
+                </p>
+              </div>
+
+              <picture className='w-full h-full'>
+                <img
+                  src={Mascot}
+                  alt='Yae Miko. Mascot for this version of Echo'
+                  className='object-cover object-center h-full w-full'
+                />
+              </picture>
+            </div>
           }
         />
         <Outputs />
